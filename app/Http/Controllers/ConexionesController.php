b9f342631f090e53c37fac32d16b54abcb8130ba
valentin22bjrs <valentinbjrs22@gmail.com>
2020-10-09 21:18:52 -0300

    Nuevo Commit

---
+++ app/Http/Controllers/ConexionesController.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConexionesController extends Controller
+{
+    
+        public function crear()
+        {
+            //nombre de la vista
+            return view ('');
+        }
+
+        public function mostrar(Request $request)
+        {
+            $request->validate([
+                'idPersona' => 'required',
+                'idAnimal' =>'required',
+                'tipo' => 'required',
+                'fechainicial' => 'required',
+                'fechafinal' => 'required';
+            ]);
+
+            $idPersona = $request->input('idPersona');
+            $idAnimal = $request->input('idAnimal');
+            $tipo = $request->input('tipo');
+            $fechainicial = $request->input('fechainicial');
+            $fechafinal = $request->input('fechafinal');
+
+            $conex  = new Conexion();
+            $conex->setIdPersona($idPersona);
+            $conex->setIdAnimal($idAnimal);
+            $conex->setTipo($tipo);
+            $conex->setFechaInicial($fechainicial);
+            $conex->setFechaFinal($fechafinal);
+            $conex->save();
+
+            //nombre de la vista
+            return view('', [ 
+            'conex' => $conex)
+             ]);
+        }
+   
+} -->
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/carga', 'ConexionesController@crear ');
+Route::post('/carga', 'ConexionesController@mostrar');
+
